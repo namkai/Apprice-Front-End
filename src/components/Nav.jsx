3586c9b38fdd317b187acce600d98fa6c6d335e7
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-01-29 15:26:45 -0800

    updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-
 const Nav = (props) => {
     return (
         <nav>
             <ul>
                 <li>
                     <div id="logo">
-                        <a href="#">Apprice</a>
+                        <Link to={"/"}>Apprice</Link>
                     </div>
                 </li>
-                <li>
-                    <Link to={"/SignUp"}></Link>
-                </li>
-                <li>
-                    <a href="news.asp">Login</a>
-                </li>
-                <li>
-                    <a href="contact.asp">Help</a>
-                </li>
+                <div id="links">
+                    <li>
+                        <Link to={"/SignUp"}>Sign Up</Link>
+                    </li>
+                    <li>
+                        <a href="news.asp">Login</a>
+                    </li>
+                    <li>
+                        <a href="contact.asp">Help</a>
+                    </li>
+                </div>
             </ul>
         </nav>
     )
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,13 +4,21 @@ import {Link} from 'react-router';
 export default class SignUp extends Component {
     constructor(props){
         super(props);
-
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        
     }
     render() {
         return (
-            <form action="">
-                <input type="text"/>
-                <button></button>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="First Name"/>
+                <input type="text" placeholder="Last Name"/>
+                <br/>
+                <input type="email"/>
+                <br/>
+                <button>Sign Up!</button>
             </form>
         )
     }
