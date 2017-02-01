96d80a462b02bf7de58351a830773457ac51faec
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2017-02-01 10:50:25 -0800

    added updated seeds

--- seeds/011products_filter_tags.js
+++ seeds/011products_filter_tags.js
@@ -7,13 +7,28 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
 
         knex('products_filter_tags').insert({
-          product_id: 10,
+          product_id: 1,
           filter_tag_id: 1
         }).returning('id'),
 
         knex('products_filter_tags').insert({
-          product_id: 11,
+          product_id: 2,
           filter_tag_id: 1
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 6,
+          filter_tag_id: 1
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 7,
+          filter_tag_id: 1
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 10,
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
@@ -22,42 +37,82 @@ exports.seed = function(knex, Promise) {
         }).returning('id'),
 
         knex('products_filter_tags').insert({
+          product_id: 11,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 12,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
           product_id: 13,
-          filter_tag_id: 1
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 14,
-          filter_tag_id: 1
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 15,
-          filter_tag_id: 1
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 16,
-          filter_tag_id: 1
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 17,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 19,
           filter_tag_id: 1
         }).returning('id'),
 
         knex('products_filter_tags').insert({
+          product_id: 20,
+          filter_tag_id: 1
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 27,
+          filter_tag_id: 1
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 71,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
           product_id: 82,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 86,
           filter_tag_id: 1
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 88,
-          filter_tag_id: 1
+          filter_tag_id: 2
         }).returning('id'),
 
         knex('products_filter_tags').insert({
           product_id: 89,
+          filter_tag_id: 2
+        }).returning('id'),
+
+        knex('products_filter_tags').insert({
+          product_id: 94,
           filter_tag_id: 1
         }).returning('id')
 
