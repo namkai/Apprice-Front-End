63c5bb7693afe044249bf010ff38c4834cc928ea
Eleanor Howard <ellie@lizthedeveloper.com>
2017-02-08 19:15:47 -0800

    testing appriceme response

--- routes/api/appriceme.js
+++ routes/api/appriceme.js
@@ -27,19 +27,20 @@ router.route('/').post(function(req, res, next){
         this.whereIn('store_id', storesInRadiusIds)
     })
         .then(function(storesProductsData){
-            let optimalStores = [];
             var returnData;
             if(numOfStores == 1){
                 storesProductsGraph.initialize(storesInRadius, selectedProducts, storesProductsData)
                 result = storesProductsGraph.oneStopSearch(selectedProductsIds)
-                optimalStores.push(result[0])
                 returnData = storesProductsGraph.convertOneStoptoJSON(result, storesProductsData)
             } else {
                 storesProductsGraph.initializeForMultiple(storesInRadius, selectedProducts, storesProductsData)
                 result = storesProductsGraph.MultipleStopSearch(numOfStores, storesInRadius, selectedProductsIds)
                 returnData = storesProductsGraph.convertOptMSTtoJSON(result, storesProductsData)
             }
-
+            returnData.forEach(function(currentStoreProduct){
+                delete currentStoreProduct.created_at;
+                delete currentStoreProduct.updated_at;
+            })
             res.json(returnData);
         })
 });
@@ -79,3 +80,19 @@ router.route('/convert').post(function (req, res, next) {
 
 
 module.exports = router;
+
+//  sample request body:
+//     {products: [
+//         {"products":
+//         {"id": 6, "upc": "015141503495", "plu": null, "name": "Eggland's Best Grade A, White, Large Eggs (dozen)", "brand_name": "Eggsland Best", "brand_type": "Grade A, White, Large", "size": "dozen", "product_image_url": "http://scene7.targetimg1.com/is/image/Target/14711485?wid=1000&hei=1000"}},
+//         {"products": { "id": 8, "upc": "037000138815", "plu": null, "name": "Tide Original Scent Liquid (75 oz.)", "brand_name": "Tide", "brand_type": "Original Scent Liquid", "size": "75 oz.", "product_image_url": "https://i5.walmartimages.com/asr/3915ec1e-7968-456a-ad1d-e0897671e7c6_1.a9dac59d32cd4344d473213c761ae62b.jpeg"}},
+//         {"products": { "id": 15, "upc": "492840220616", "plu": null, "name": "Horizon Organic 1% Lowfat Milk (Gallon)", "brand_name": "Horizon Organic", "brand_type": "1% Lowfat Milk", "size": "Gallon", "product_image_url": "http://scene7.targetimg1.com/is/image/Target/14937615?wid=1000&hei=1000"}},
+//         {"products": { "id": 47, "upc": null, "plu": "94050", "name": "Cantaloupe (large) [organic]", "brand_name": null, "brand_type": null, "size": "large", "product_image_url": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQzAZEG33Tn3FOuGvmwlzg6IUxrUpdgOX26xMrI5gAZU3SQ1MmAWQ"}},
+//         {"products": {     "id": 84,     "upc": "492310012048",     "plu": null,     "name": "Post Fruity Pebbles (11 oz.)",     "brand_name": "Post",     "brand_type": "Fruity Pebbles (11 oz.)",     "size": "11 oz.",     "product_image_url": "http://target.scene7.com/is/image/Target/14775577?wid=450&hei=450&fmt=pjpeg"}},
+//         {"products": { "id": 91, "upc": "073435093305", "plu": null, "name": "King's Hawaiian Sweet Sliced Bread (16 oz.)", "brand_name": "King's Hawaiian", "brand_type": "Sweet Sliced Bread (16 oz.)", "size": "16 oz.", "product_image_url": "http://target.scene7.com/is/image/Target/16229584?wid=450&hei=450&fmt=pjpeg"}} ],
+//     filteredStores: {"data": [{    "id":1, "name":"Whole Foods Market", "address":"399 4th St, San Francisco, CA 94107", "phone_number":"(415) 618-0066","store_url":"http://www.wholefoodsmarket.com/stores/soma","store_image_url":"http://www.trbimg.com/img-568288cb/turbine/ct-whole-foods-overcharging-nyc-customers-20151229","latitude":"37.780875000000", "longitude":"-122.399641000000", "created_at":"2016-11-14T20:26:16.000Z", "updated_at":"2016-11-14T20:26:16.000Z"     },
+//          {"id":2,"name":"Bristol Farms","address":"Westfield San Francisco Centre, 845 Market St #10, San Francisco, CA 94103","phone_number":"(415) 979-0106","store_url":"http://www.bristolfarms.com/locations/?loc=76","store_image_url":"http://comediva.com/images/stories/bristol2222012.jpg","latitude":"37.784100000000", "longitude":"-122.406144990000", "created_at":"2016-11-14T20:26:16.000Z", "updated_at":"2016-11-14T20:26:16.000Z"},
+//               {"id":3,         "name":"Target",         "address":"Metreon, 789 Mission St, San Francisco, CA 94103",         "phone_number":"(415) 343-6272"         ,"store_url":"http://www.target.com/sl/san-francisco-central/2766#?afid=storeloc&cpng=CA&Inm=san-francisco-central_2766"         ,"store_image_url":"http://www.savingadvice.com/articles/wp-content/uploads/2015/09/237014_Martinsburg_VA_Target_38a77055-e931-4f2c-8455-bf8c7c8925f4-prv.jpg"         ,"latitude":"37.784736000000"         ,"longitude":"-122.403691000000"         ,"created_at":"2016-11-14T20:26:16.000Z"         ,"updated_at":"2016-11-14T20:26:16.000Z"     },
+//                    {         "id":4,"name":"Safeway"         ,"address":"298 King St, San Francisco, CA 94107"         ,"phone_number":"(415) 633-1001"         ,"store_url":"http://local.safeway.com/ca/san-francisco-2606.html?utm_source=G&utm_medium=Maps&utm_campaign=G+Places"         ,"store_image_url":"http://www.operating-hours.com/wp-content/uploads/2015/08/SAFEWAY-STORE.jpg" ,"latitude":"37.776667000000" ,"longitude":"-122.394109000000" ,"created_at":"2016-11-14T20:26:16.000Z"         ,"updated_at":"2016-11-14T20:26:16.000Z"     },
+//                         {         "id": 7,         "name": "Trader Joe's",         "address": "555 9th St, San Francisco, CA 94103",         "phone_number": "(415) 863-1292",         "store_url": "http://www.traderjoes.com/",         "store_image_url": "http://www.traderjoes.com/images/announcement/778-Davie-Store.jpg",         "latitude": 37.7707200,         "longitude": -122.4075760      } ]}
+// }
