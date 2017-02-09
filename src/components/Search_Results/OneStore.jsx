8ec9dc57d788f65578a0fd7b05bb5047002968ca
Namkai Fairfield <namkai.fairfield@gmail.com>
2017-02-08 19:52:22 -0800

    fixed

--- src/components/Search_Results/OneStore.jsx
+++ src/components/Search_Results/OneStore.jsx
@@ -11,11 +11,11 @@ export default class OneStore extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="column column-12 full">
-                        <div className="column column-4 full">
+                    <div className="column column-12">
+                        <div className="column column-4" id="OneStore-Picture">
                             <h2>I'm gonna be a picture Yo</h2>
                         </div>
-                        <div className="column column-4 full">
+                        <div className="column column-4">
                             <h2>{this.props.data.products[0].name}</h2>
                             <hr className="list-hr"/>
                             <Grocery_List groceries={this.props.data.products[0].products}/>
